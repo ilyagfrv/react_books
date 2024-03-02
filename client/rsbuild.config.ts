b204ec118a7aae3f8dd93be371fe95f48c9473e1
igfrv <ilyagafarov@bk.ru>
2024-03-02 19:29:09 +0600

    minor changes

--- client/rsbuild.config.ts
+++ client/rsbuild.config.ts
@@ -1,6 +1,10 @@
-import { defineConfig } from '@rsbuild/core';
-import { pluginReact } from '@rsbuild/plugin-react';
+import { defineConfig } from '@rsbuild/core'
+import { pluginReact } from '@rsbuild/plugin-react'
 
 export default defineConfig({
   plugins: [pluginReact()],
-});
+  html: {
+    title: 'Book Library',
+    favicon: './src/assets/icons/icon.svg',
+  },
+})
